61f6db5b881255bb7751bed543153db0850258c5
Héctor de la Rosa <hoftherose@gmail.com>
2021-09-25 15:53:09 -0500

    Add pubsub to billing (permissions error)

--- billingManager/infrastructure/main.tf
+++ billingManager/infrastructure/main.tf
@@ -14,3 +14,34 @@ resource "google_project" "billing_manager_template" {
 
     billing_account  = data.google_billing_account.billing_account.id
 }
+
+resource "google_billing_budget" "budget_alert" {
+    billing_account = data.google_billing_account.billing_account.id
+    display_name = "Example Billing Budget Template"
+    amount {
+        specified_amount {
+        currency_code = "MEX"
+        units = "100"
+        }
+    }
+    threshold_rules {
+        threshold_percent =  0.1
+    }
+    threshold_rules {
+        threshold_percent =  0.3
+    }
+    threshold_rules {
+        threshold_percent =  1.0
+    }
+    threshold_rules {
+        threshold_percent = 0.5
+        spend_basis = "FORECASTED_SPEND"
+    }
+    threshold_rules {
+        threshold_percent = 1.0
+        spend_basis = "FORECASTED_SPEND"
+    }
+    all_updates_rule {
+        pubsub_topic = resource.google_pubsub_topic.billing_pubsub_template.id
+    }
+}
---
+++ billingManager/infrastructure/pubsub.tf
@@ -0,0 +1,5 @@
+resource "google_pubsub_topic" "billing_pubsub_template" {
+    project = google_project.billing_manager_template.project_id
+
+    name = "BillingPubsubTemplate"
+}
